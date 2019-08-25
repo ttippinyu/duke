67b3f3a40518b87a3442c02e99ebd4ffd8be28b4
ttippinyu <StartNow25>
2019-08-25 19:17:12 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+
+    private static void printIndent(String line) {
+        System.out.println("    " + line);
     }
+
+    private static void printUnderline() {
+        printIndent("____________________________________________________________");
+    }
+
+    private static void startDuke() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printUnderline();
+                printIndent("Bye. Hope to see you again soon!");
+                printUnderline();
+                return;
+            } else {
+                printUnderline();
+                printIndent(input);
+                printUnderline();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        printUnderline();
+        printIndent("Hello! I'm Duke");
+        printIndent("What can I do for you?");
+        printUnderline();
+        startDuke();
+    }
+
 }
